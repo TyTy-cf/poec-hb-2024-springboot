1acb3bee6131fe4f52eb1bc141febb2ec2262cac
Kevin Tourret <kevin@drosalys.fr>
2024-01-04 14:49:19 +0100

    update exo : serialization

--- src/main/java/fr/poec/springboot/instant_faking/repository/UserOwnGameRepository.java
+++ src/main/java/fr/poec/springboot/instant_faking/repository/UserOwnGameRepository.java
@@ -4,6 +4,8 @@ import fr.poec.springboot.instant_faking.entity.UserOwnGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserOwnGameRepository extends JpaRepository<UserOwnGame, Long> {
 
